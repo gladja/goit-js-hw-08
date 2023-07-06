f2f688ced44496bac2ecb7ae964687a85b8fa455
gladja <user@MacBook-Pro-user.local>
2023-07-06 23:03:42 +0300

    task-01 and task-03

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.css";
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+function createdElementGallery(galleryItems) {
+  return galleryItems.map(itm => {
+    return `
+   <li class='gallery__item'>
+   <a class='gallery__link' href='${itm.original}'>
+     <img
+       class='gallery__image'
+       src='${itm.preview}'
+       alt='${itm.description}'
+     />
+   </a>
+ </li>
+  `;
+  }).join('');
+}
+
+const element = createdElementGallery(galleryItems);
+// console.log(element);
+galleryEl.insertAdjacentHTML('beforeend', element);
+
+let gallery = new SimpleLightbox('.gallery a',
+  {
+    fadeSpeed: 300,
+    animationSpeed: 250,
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,62 @@
+const inputEmailEl = document.querySelector('input');
+const textareaEl = document.querySelector('textarea');
+const btnEl = document.querySelector('button');
+const feedbackFormEl = document.querySelector('.feedback-form');
 
+
+// inputEmailEl.addEventListener('input', onEmailValue);
+// textareaEl.addEventListener('input', onTextValue);
+btnEl.addEventListener('click', onSubmitValue);
+feedbackFormEl.addEventListener('input', onValue);
+
+function onValue(event) {
+  // console.log(event.target.tagName)
+  if(event.target.tagName === 'INPUT') {
+    const email = event.target.value;
+    localStorage.setItem('email_key', email);
+    console.log(email);
+  }
+}
+
+
+
+
+
+
+
+function onEmailValue(event) {
+  const email = event.target.value;
+
+  localStorage.setItem('email_key', email);
+}
+
+function onTextValue(event) {
+  const text = event.target.value;
+
+  localStorage.setItem('text_key', text);
+}
+
+getValueLocalStorage();
+function getValueLocalStorage() {
+  const saveEmail = localStorage.getItem('email_key');
+  const saveText = localStorage.getItem('text_key');
+
+  // console.log(savedEmail);
+  if (saveEmail && saveText) {
+    inputEmailEl.value = saveEmail;
+    textareaEl.value = saveText;
+  }
+}
+
+// let emailSav = ''
+function onSubmitValue(event) {
+  event.preventDefault();
+
+   // emailSav = localStorage.getItem('email_key')
+
+   console.log(inputEmailEl.value)
+  localStorage.removeItem('email_key');
+  inputEmailEl.value = ''
+}
+
+// console.log(emailSav);
