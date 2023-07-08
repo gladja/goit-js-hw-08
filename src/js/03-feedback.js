f44a42dcb48a9dd649ed730db51064016d34b4de
gladja <user@MacBook-Pro-user.local>
2023-07-08 15:53:25 +0300

    task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,62 +1,63 @@
+import throttle from 'lodash.throttle';
+
 const inputEmailEl = document.querySelector('input');
 const textareaEl = document.querySelector('textarea');
 const btnEl = document.querySelector('button');
 const feedbackFormEl = document.querySelector('.feedback-form');
 
 
-// inputEmailEl.addEventListener('input', onEmailValue);
-// textareaEl.addEventListener('input', onTextValue);
 btnEl.addEventListener('click', onSubmitValue);
-feedbackFormEl.addEventListener('input', onValue);
+feedbackFormEl.addEventListener('input', throttle(onValue, 500));
+
+const objState = {};
+const STORAGE_LOCAL = 'feedback-form-state';
 
 function onValue(event) {
-  // console.log(event.target.tagName)
-  if(event.target.tagName === 'INPUT') {
-    const email = event.target.value;
-    localStorage.setItem('email_key', email);
-    console.log(email);
-  }
-}
-
-
-
-
-
-
-
-function onEmailValue(event) {
-  const email = event.target.value;
-
-  localStorage.setItem('email_key', email);
-}
-
-function onTextValue(event) {
-  const text = event.target.value;
-
-  localStorage.setItem('text_key', text);
+  objState[event.target.name] = event.target.value;
+  // console.log(obj);
+  localStorage.setItem(STORAGE_LOCAL, JSON.stringify(objState));
 }
 
 getValueLocalStorage();
 function getValueLocalStorage() {
-  const saveEmail = localStorage.getItem('email_key');
-  const saveText = localStorage.getItem('text_key');
+  const localState = JSON.parse(localStorage.getItem(STORAGE_LOCAL));
+  // console.log(newEL);
+  if (!localState) return;
 
-  // console.log(savedEmail);
-  if (saveEmail && saveText) {
-    inputEmailEl.value = saveEmail;
-    textareaEl.value = saveText;
+  if (localState.email) {
+    inputEmailEl.value = localState.email;
+  }
+  if (localState.message) {
+    textareaEl.value = localState.message;
   }
 }
 
 // let emailSav = ''
 function onSubmitValue(event) {
   event.preventDefault();
+  // event.currentTarget.reset()
+  const localState = JSON.parse(localStorage.getItem(STORAGE_LOCAL));
+  console.log(localState);
 
-   // emailSav = localStorage.getItem('email_key')
-
-   console.log(inputEmailEl.value)
-  localStorage.removeItem('email_key');
-  inputEmailEl.value = ''
+  localStorage.removeItem(STORAGE_LOCAL);
+  inputEmailEl.value = '';
+  textareaEl.value = '';
 }
 
-// console.log(emailSav);
+
+
+
+
+
+// inputEmailEl.addEventListener('input', onEmailValue);
+// textareaEl.addEventListener('input', onTextValue);
+// function onEmailValue(event) {
+//   const email = event.target.value;
+// }
+// function onTextValue(event) {
+//   const message = event.target.value;
+//   // localStorage.setItem('feedback-form-state', JSON.stringify(message))
+// }
+
+
+// localStorage.setItem('feedback-form-state', `{"email":"${email}", "message":"${message}"}`)
