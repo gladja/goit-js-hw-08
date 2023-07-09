bddf5bd8f18c58c829919ef1dcffc317210dec6f
gladja <user@MacBook-Pro-user.local>
2023-07-09 09:37:49 +0300

    refactor task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,26 +2,24 @@ import throttle from 'lodash.throttle';
 
 const inputEmailEl = document.querySelector('input');
 const textareaEl = document.querySelector('textarea');
-const btnEl = document.querySelector('button');
 const feedbackFormEl = document.querySelector('.feedback-form');
 
-
-btnEl.addEventListener('click', onSubmitValue);
 feedbackFormEl.addEventListener('input', throttle(onValue, 500));
+feedbackFormEl.addEventListener('submit', onSubmitValue);
 
 const objState = {};
 const STORAGE_LOCAL = 'feedback-form-state';
 
 function onValue(event) {
   objState[event.target.name] = event.target.value;
-  // console.log(obj);
   localStorage.setItem(STORAGE_LOCAL, JSON.stringify(objState));
+
+  //? console.log(event.target.value, 'VS', feedbackFormEl.elements.email.value);
 }
 
 getValueLocalStorage();
 function getValueLocalStorage() {
   const localState = JSON.parse(localStorage.getItem(STORAGE_LOCAL));
-  // console.log(newEL);
   if (!localState) return;
 
   if (localState.email) {
@@ -35,14 +33,74 @@ function getValueLocalStorage() {
 // let emailSav = ''
 function onSubmitValue(event) {
   event.preventDefault();
-  // event.currentTarget.reset()
+  event.currentTarget.reset()
+
   const localState = JSON.parse(localStorage.getItem(STORAGE_LOCAL));
   console.log(localState);
 
   localStorage.removeItem(STORAGE_LOCAL);
-  inputEmailEl.value = '';
-  textareaEl.value = '';
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const inputEmailEl = document.querySelector('input');
+// const textareaEl = document.querySelector('textarea');
+// const btnEl = document.querySelector('button');
+// const feedbackFormEl = document.querySelector('.feedback-form');
+//
+//
+// btnEl.addEventListener('click', onSubmitValue);
+// feedbackFormEl.addEventListener('input', throttle(onValue, 500));
+//
+// const objState = {};
+// const STORAGE_LOCAL = 'feedback-form-state';
+//
+// function onValue(event) {
+//   objState[event.target.name] = event.target.value;
+//   console.log(objState);
+//   localStorage.setItem(STORAGE_LOCAL, JSON.stringify(objState));
+// }
+//
+// getValueLocalStorage();
+// function getValueLocalStorage() {
+//   const localState = JSON.parse(localStorage.getItem(STORAGE_LOCAL));
+//   // console.log(newEL);
+//   if (!localState) return;
+//
+//   if (localState.email) {
+//     inputEmailEl.value = localState.email;
+//   }
+//   if (localState.message) {
+//     textareaEl.value = localState.message;
+//   }
+// }
+//
+// // let emailSav = ''
+// function onSubmitValue(event) {
+//   event.preventDefault();
+//   // event.currentTarget.reset()
+//   const localState = JSON.parse(localStorage.getItem(STORAGE_LOCAL));
+//   console.log(localState);
+//
+//   localStorage.removeItem(STORAGE_LOCAL);
+//   inputEmailEl.value = '';
+//   textareaEl.value = '';
+// }
 
 
 
